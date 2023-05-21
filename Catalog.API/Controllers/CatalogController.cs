ac59f6f6ca9fc14a55870c9e61781d56ba034c7a
Vsevolod Zakharchenko <vzakha@softserveinc.com>
2023-05-21 14:32:32 +0300

    init docker-compose

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(ILogger<CatalogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
