44be06f161d8c8ab7b96b6c8dcd41a9fd131d0c1
Vsevolod Zakharchenko <vzakha@softserveinc.com>
2023-05-24 01:39:33 +0300

    + init Order.API

---
+++ Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
