447b953878f9de57242a202fa767fdd783419c30
Vsevolod Zakharchenko <vzakha@softserveinc.com>
2023-06-06 22:58:41 +0300

    init Discount Grpc

---
+++ Discount.gRPC/Program.cs
@@ -0,0 +1,38 @@
+using Discount.gRPC.Extensions;
+using Discount.gRPC.Repositories;
+using Discount.gRPC.Services;
+using Discount.Grpc.Protos;
+
+namespace Discount.gRPC
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Additional configuration is required to successfully run gRPC on macOS.
+            // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            // Add services to the container.
+            builder.Services.AddGrpc();
+
+            var app = builder.Build();
+
+            app.MigrateDatabase<Program>();
+
+            // Configure the HTTP request pipeline.
+            app.MapGrpcService<DiscountService>();
+            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+            app.Run();
+
+            
+        }
+    }
+}
+
