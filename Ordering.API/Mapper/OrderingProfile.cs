47683fdf5fbdc834dfa2bcbe77129c1a20099388
Vsevolod Zakharchenko <vzakha@softserveinc.com>
2023-06-09 21:05:37 +0300

    ++ Consume Event

---
+++ Ordering.API/Mapper/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Messages.Events;
+using Ordering.Application.Features.Commands.CheckoutOrder;
+using AutoMapper;
+
+namespace Ordering.API.Mapper
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
